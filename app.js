5273f3730ea81ee85a3e3fd095965d97bec3b1f4
huzaifasarmad1 <73124077+huzaifasarmad1@users.noreply.github.com>
2021-10-17 10:20:15 +0500

    last commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var transporter = nodemailer.createTransport({
     pass: '' //add pass for that email
   }
 });
-mongoose.connect('mongoose connection string here', { useNewUrlParser: true });
+mongoose.connect('', { useNewUrlParser: true });
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -47,15 +47,10 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 //run a job every middnight
-cron.schedule('0 16 * * *', () => {
-  console.log('running a task every midnight');
+cron.schedule('0 3 * * 2-6', () => {
+  console.log('running a task every 3am');
   func();
 });
-
-console.log('upside func')
-// setIntervalAsync(
-//   () => {console.log('Hello')
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const asyncIntervals = [];
 const runAsyncInterval = async (cb, interval, intervalIndex) => {
@@ -81,8 +76,8 @@ const clearAsyncInterval = (intervalIndex) => {
   }
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////
+var token='';
 let func = async () => {
-  console.log('inside func')
   var iterator = 0;
   // here we need to remove the extra tickers  okay..let me make the list on excel yes you can give me an excel file same as you gave me before
   let data = ["GHSI", "GSV", "SYN", "TRX", "ACST", "ITP", "PTN", "NAK", "ADXS", "GTE", "ASRT", "NSPR", "TLGT", "BIOL", "CSCW", "GPL", "XPL", "CTRM", "CFMS", "OGEN", "KIQ", "NAKD", "DNN", "INUV", "NAOV", "MUX", "ADMP", "NBY", "TBLT", "URG", "HSTO", "SHIP", "DFFN", "UXIN", "AEZS", "TNXP", "AIKI", "MTNB", "BORR", "RGLS", "ONTX", "INPX", "ZSAN", "PTE", "SNDL", "MKD", "BXRX", "CHEK", "CIDM", "ISR", "ASM", "NOVN", "NVCN", "UAMY", "CEI", "MOTS", "HDSN", "ATIF", "POAI", "DYNT", "TXMD", "PULM", "LKCO", "NEPT", "BRQS", "LPCN", "AVGR", "NGD", "ITRM", "NXTD", "TGB", "NMTR", "GNUS", "CCO", "XSPA", "IBIO", "GSAT", "USWS", "VEON", "CLBS", "GERN", "TMBR", "PHUN", "ESGC", "ZOM", "HEPA", "GTT", "ACRX", "DGLY", "TYME", "JFU", "CTXR", "ATHX", "SINT", "CLSN", "JAGX", "CIG", "UEC", "QD", "LYG", "STRM", "TRVN", "CRBP", "BEST", "REI", "TTOO", "HOFV", "HOTH", "TOPS", "DHY", "METX", "NGL", "ABEO", "VTGN", "TGC", "TRCH", "TTI", "ABEV", "MICT", "NBEV", "LAIX", "VTVT", "EXPR", "CHS", "ATOS", "SESN", "BOXL", "LQDA", "IAG", "NAT", "MARK", "OGI", "INFI", "DHF", "KOS", "TELL", "GNW", "LGHL", "WTRH", "IDEX", "VISL", "LLNW", "WTI", "COMS", "CIK", "XXII", "VBIV", "KXIN", "RRD", "SENS", "AHT", "REPH", "SPPI", "QEP", "AKBA", "RIG", "AUUD", "QTT", "UGP", "NXE", "SAN", "TOUR", "MBIO", "DSS", "CPG", "OPTT", "TRXC", "ABUS", "HMY", "ENLC", "EVFM", "IVR", "MREO", "BRFS", "OBSV", "NOK", "JE", "CDEV", "AUY", "MFA", "SWN", "DPW", "BBD", "RIGL", "NYMT", "PAVM", "BTU", "XTNT", "TRVG", "TEF", "WATT", "BTG", "CRNT", "ETM", "BGCP", "OPK", "ITUB", "SRTS", "GGB", "FLDM", "AQMS", "AIV", "SOS", "ERF", "HLX", "ASMB", "TCDA", "AFI", "EFOI", "UUUU", "AREC", "ZIOP", "JG", "CNDT", "AMRX", "BBVA", "LIXT", "DHT", "LTRPA", "AFMD", "MNKD", "BKD", "SVM", "SIRI", "SID", "GLOG", "EXK", "CLNY", "CBAT", "CLVS", "SAND", "HMHC", "WIT", "WWR", "CERS", "SOLO", "KGC", "VET", "ALTO", "HL", "SBS", "CX", "ENBL", "MGI", "AMRN", "FRSX", "EBON", "HEXO", "KNDI", "AQB", "AGI", "VXRT", "AYRO", "CXW", "GEO", "TWO", "ETRN", "PSEC", "ZIXI", "PTEN", "CVE", "TV", "KALA", "GPRO", "FSM", "UWMC", "ASX", "ADT", "ET", "COTY", "MOGO", "GEL", "PBR", "ENDP", "AMC", "UAVS", "GFI", "FTI", "MBT", "EQX", "WIMI", "SILV", "NLY", "NG", "KOPN", "PAA", "PBI", "PAGP", "KODK", "DVAX", "ERJ", "AM", "BCS", "LOTZ", "AR", "CDE", "IRWD", "PGRE", "CGEN", "WPRT", "CLOV", "KMPH", "NPTN", "RRC", "SRNE", "GEVO", "PVG", "BNGO", "UMC", "INFN", "NNDM", "TWLVU", "CFII", "OR", "MACQU", "BB", "SBEAU", "DRH", "NRZ", "CRDF", "RUBY", "SLAMU", "CRON", "EGO", "PCG", "ACB", "AUVI", "HEC", "TEVA", "BCRX", "SRNGU", "ING", "AACQ", "AMCR", "OCGN", "SHLX", "ALUS", "MRO", "INO", "ZNGA", "SGMO", "NGAC", "CIM", "SDC", "AJAX", "SFTW", "F", "RTP", "EAF", "FNB", "ACIC", "UNIT", "RAAC", "GSAH", "ATNX", "MTG", "NYCB", "BDN", "DB", "FOLD", "LUMN", "RMO", "QRTEA", "GLUU", "ERIC", "GE", "CNX", "FRX", "AMX", "GOEV", "MAC", "CLNE", "SUNW", "SHO", "VG", "CLF", "ISBC", "HIMX", "ROOT", "BTWN", "IPOF", "AMRS", "KPTI", "NPA", "SM", "KAR", "CDXC", "HOL", "AUPH", "NBLX", "XL", "SSRM", "PBF", "APLE", "PRMW", "DOYU", "TWNK", "CS", "HPE", "COMM", "INSG", "VLDR", "SABR", "KMI", "VTRS", "MVIS", "CBD", "GTES", "MIK", "LU", "SY", "RLGY", "NOV", "HYLN", "CAN", "M", "BFT", "HBAN", "MDRX", "UBS", "CCJ", "SKT", "SLM", "MOMO", "GPK", "AGNC", "CLDR", "WKHS", "ACI", "VIAV", "FHN", "MUR", "STLA", "GMBL", "HST", "X", "IBN", "WES", "ABR", "KOSS", "TAK", "GT", "VALE", "FCEL", "DOC", "BPY", "INFY", "FLR", "VOD", "VST", "RLX", "YSG", "HBI", "GHVI", "EQT", "APHA", "PBCT", "AG", "ESI", "NKLA", "FLEX", "UA", "TGNA", "IGT", "KIM", "TROX", "SBSW", "BOX", "IPOE", "JBLU", "COG", "LTHM", "SVMK", "GOLD", "LAC", "CYTK", "VUZI", "EQNR", "BFLY", "HRB", "FEYE", "ORI", "RIDE", "CNP", "NLOK", "WISH", "BRX", "APA", "SU", "EB", "MFC", "AU", "DM", "KEY", "MAT", "OUT", "RDN", "WEN", "PS"];
@@ -98,96 +93,81 @@ let func = async () => {
   let increased_tickers = [];
   // for (const iterator of data) {
   var interval = setAsyncInterval(async func2 => {
-    console.log('inside set interval')
     var ticker = {};
     try {
       let response1 = await fetch(
         //${data[iterator]}
-        `https://api.tiingo.com/tiingo/daily///${data[iterator]}/prices?token=//`, {
+        `https://api.tiingo.com/tiingo/daily///${data[iterator]}/prices?token=${token}`, {
         method: 'GET',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         },
       });
-      let json1 = await response1.json();
-      // ticker = { date: json1[0].date, ticker: data[iterator], closed_price: json1[0].close, Volume:json1[0].volume }
-      console.log(json1)
-      if (json1.length > 0) {
-        console.log('inside fetch if')
-        //second one
-        let response2 = await fetch(
-          `https://api.tiingo.com/tiingo/daily///${data[iterator]}/prices?startDate=${oldDate}&endDate=${json1[0].date}&token=//`, {
+      let latestData = await response1.json();
+     if(latestData.detail=='Error: You have run over your monthly bandwidth allocation. Please upgrade at https://api.tiingo.com/pricing to have your limits increased.'){
+        token=''
+        func();
+      }
+      if (latestData.length > 0) {
+     let response2 = await fetch(
+          `https://api.tiingo.com/tiingo/daily///${data[iterator]}/prices?startDate=${oldDate}&endDate=${latestData[0].date}&token=${token}`, {
           method: 'GET',
           headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json'
           },
         });
-        let json2 = await response2.json();
-        if (json2.length == 0) {
-          console.log('json2 have zero length')
+        let allData = await response2.json();
+        if (allData.length == 0) {
+          console.log('allData have zero length')
         }
         else {
           var sum1 = 0;
-          console.log('json2.length', json2.length)
-          for (i = json2.length - 100; i < json2.length; i++) {
-            sum1 = sum1 + json2[i].close
+         for (i = allData.length - 100; i < allData.length; i++) {
+            sum1 = sum1 + allData[i].close
           }
 
           let price = sum1 / 100;
-          if (json2.length == 0) {
+          if (allData.length == 0) {
             price = 0;
           }
-          console.log('Todays average is ' + price);
-          console.log('Todays close is ' + json1[0].close);
+         
           var yesterdayclose = 0;
-          for (i = json2.length - 2; i < json2.length - 1; i++) {
-            console.log(json2[i].date)
-            console.log('json 2 ')
-            yesterdayclose = json2[i].close
-            console.log('inside for yester close')
-            console.log(yesterdayclose + 'is yesterdays close')
-          }
+          for (i = allData.length - 2; i < allData.length - 1; i++) {
+    yesterdayclose = allData[i].close
+    }
           var sum2 = 0;
           var yesterdayaverage = 0;
-          for (i = json2.length - 101; i < json2.length - 1; i++) {
-            sum2 = sum2 + json2[i].close
+          for (i = allData.length - 101; i < allData.length - 1; i++) {
+            sum2 = sum2 + allData[i].close
           }
           yesterdayaverage = sum2 / 100;
-          console.log(yesterdayaverage + 'is yestedays average')
-          if (price > json1[0].close) {
-            console.log('Todays average is greater than close today')
+         
+          if (price > latestData[0].close) {
+            
             if (yesterdayaverage < yesterdayclose) {
-              console.log('its a crossover cz today avg>close yesterday close>avg')
-              const ticker = { date: json1[0].date, ticker: data[iterator], average: price, closed_price: json1[0].close }
-              console.log(ticker);
-              increased_tickers.push(ticker);
+              const ticker = { date: latestData[0].date, ticker: data[iterator], average: price, closed_price: latestData[0].close }
+           increased_tickers.push(ticker);
               const db = await TickerSchema.create(ticker);
             }
             else {
-              // console.log(json1[0].close+'h'+json1[0].date)
-              console.log('its not a crossover1')
-              
-            }
+            
+              console.log('its not a crossover')
+         }
           }
           else {
-            if (price < json1[0].close) {
+            if (price < latestData[0].close) {
               if (yesterdayaverage > yesterdayclose) {
-                console.log('its a crossover cz today avg<close yesterday close<avg')
-                ticker = { date: json1[0].date, ticker: data[iterator], average: price, closed_price: json1[0].close }
-                console.log(ticker);
-                increased_tickers.push(ticker);
+                ticker = { date: latestData[0].date, ticker: data[iterator], average: price, closed_price: latestData[0].close }
+     increased_tickers.push(ticker);
                 const db = await TickerSchema.create(ticker);
               }
               else {
-                // console.log(json1[0].close+'j'+json1[0].date)
-                console.log('its not a crossover2')
-           
+          console.log('its not a crossover')
               }
             }
           }
-         
         }
       } else {
         console.log('No Data in first api');
@@ -195,8 +175,7 @@ let func = async () => {
     } catch (error) {
       console.error(error);
     }
-    console.log('at the end of set interval')
-    iterator = iterator + 1;
+   iterator = iterator + 1;
     if (iterator == data.length) {
       clearAsyncInterval(interval);
       console.log('iterator cleared')
@@ -247,13 +226,12 @@ let func = async () => {
     var mailOptions = {
       from: '"Tickers ?" <no-reply@tickers.com>', // sender address
       to: '', // list of receivers( , separated here)
-      subject: 'Here is the daily report of tickers increased', // Subject line
+      subject: 'New York Stock Exchange Average', // Subject line
       html: email_html // html body
   
     };
     // send mail with defined transport object
     transporter.sendMail(mailOptions, function (error, info) {
-      console.log('inside transporter')
       if (error) {
         return console.log(error);
       }
@@ -262,13 +240,11 @@ let func = async () => {
     }
   }, 23000);
   // };
-  console.log('after interval')
  
+  
 }
 func();
-// }, 10000
 
-// )
 function formatDate(date) {
   var d = new Date(date),
     month = '' + (d.getMonth() + 1),
